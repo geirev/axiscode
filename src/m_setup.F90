b48115897255c2f658ff9bea07b4b9d583a9105b
geirev <geir.evensen@gmail.com>
2023-06-30 12:07:28 +0200

    New filepaths

--- src/m_setup.F90
+++ src/m_setup.F90
@@ -1,13 +1,15 @@
 module m_setup
-   character(len=100) :: dir_project='/home/geve/Dropbox/Axis_project'
-   character(len=3)   :: subdir_acc='acc'  ! input acceleration data
-   character(len=3)   :: subdir_pos='pos'  ! input position data
-   character(len=8)   :: subdir_accbias='acc_bias'
-   character(len=8)   :: subdir_posbias='pos_bias'
-   character(len=7)   :: subdir_foot='foot_FL'
-   character(len=65)  :: poscorfile= '/home/geve/Dropbox/Axis_project/original_data/X2_tillegg_1-67.txt'
-   character(len=62)  :: footcorfile='/home/geve/Dropbox/Axis_project/foot_frame/foot_correction.txt'
-   character(len=39)  :: accposdir='/home/geve/Dropbox/Axis_project/accpos/'
-   character(len=44)  :: accposdirorig='/home/geve/Dropbox/Axis_project/accposfull/'
-   character(len=44)  :: accposdirfilt='/home/geve/Dropbox/Axis_project/accposfilt/'
+   character(len=51) :: dir_project='/home/AD.NORCERESEARCH.NO/geev/Dropbox/Axis_project'
+   character(len=3)  :: subdir_acc='acc'  ! input acceleration data
+   character(len=3)  :: subdir_pos='pos'  ! input position data
+   character(len=8)  :: subdir_accbias='acc_bias'
+   character(len=8)  :: subdir_posbias='pos_bias'
+   character(len=7)  :: subdir_foot='foot_FL'
+   character(len=85) :: poscorfile=&
+       '/home/AD.NORCERESEARCH.NO/geev/Dropbox/Axis_project/original_data/X2_tillegg_1-67.txt'
+   character(len=82) :: footcorfile='/home/AD.NORCERESEARCH.NO/geev/Dropbox/Axis_project/foot_frame/foot_correction.txt'
+   character(len=59) :: accposdir='/home/AD.NORCERESEARCH.NO/geev/Dropbox/Axis_project/accpos/'
+   character(len=64) :: accposdirorig='/home/AD.NORCERESEARCH.NO/geev/Dropbox/Axis_project/accposfull/'
+   character(len=64) :: accposdirfilt='/home/AD.NORCERESEARCH.NO/geev/Dropbox/Axis_project/accposfilt/'
+   character(len=68) :: accposdirfullfilt='/home/AD.NORCERESEARCH.NO/geev/Dropbox/Axis_project/accposfullfilt/'
 end module
